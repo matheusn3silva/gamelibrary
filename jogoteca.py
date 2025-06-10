7f71e7b57c9d9ebd3f90e9565d01b5238ef74563
Matheus TSI <matheus.nascimento.tsi@gmail.com>
2025-06-10 07:53:38 -0300

    Página para adicionar jogos

--- jogoteca.py
+++ jogoteca.py
@@ -9,7 +9,7 @@ class Jogo:
 app = Flask(__name__)
 
 @app.route('/')
-def ola():
+def gameList():
     jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
     jogo2 = Jogo('Stardew Valley', 'Simulação', 'Computador')
     jogo3 = Jogo('Remnant from the Ashes', 'Souls Like', 'Xbox')
@@ -17,5 +17,10 @@ def ola():
     listaJogos = [jogo1, jogo2, jogo3]
 
     return render_template('list.html', title='Jogos', jogos=listaJogos)
+
+@app.route('/new')
+def newGame():
+
+    return render_template('new.html', title='Novo Jogo')
 
 app.run(host='0.0.0.0')
